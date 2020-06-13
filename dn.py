0efe68ac86271b184c648c815d3cd4a504310511
William Wade <wadewa0352@yahoo.com>
2020-06-13 12:01:37 -0400

    Updated code for last section

--- dn.py
+++ dn.py
@@ -5,38 +5,35 @@ import pdfplumber
 from collections import OrderedDict
 from PyPDF2 import PdfFileReader
 
-def getfiles(filePath):
-    pdfFilesToProcess = []
-    
-    for root,directoryNames, fileNames in os.walk(filePath):        
-        for fileName in fileNames:
-            if fileName.endswith('.pdf'):
-                pdfFilesToProcess.append(root + "\\" + fileName)
-
-    return pdfFilesToProcess
+def getfiles():
+    fls = []
+    for d, sd, fl in os.walk('.'):
+        for f in fl:
+            fls.append(os.path.join(d, f))
+    return list(filter(lambda f: '.pdf' in f, fls))
 
 def readini(fname):
-    # Instruction(s) to read an .ini file go here (and/or below)...
-    return
+    lst = []
+    with open(fname, 'r') as fh:  
+        for l in fh:
+            lst.append(l.rstrip(os.linesep))
+    return lst
 
 def getformfields(obj, tree=None, retval=None, fileobj=None):
-    """
-    Extracts field data if this PDF contains interactive form fields.
-    The *tree* and *retval* parameters are for recursive use.
-
-    :param fileobj: A file object (usually a text file) to write
-        a report to on all interactive form fields found.
-    :return: A dictionary where each key is a field name, and each
-        value is a :class:`Field<PyPDF2.generic.Field>` object. By
-        default, the mapping name is used for keys.
-    :rtype: dict, or ``None`` if form data could not be located.
-    """
-    fieldAttributes = { '/FT': 'Field Type', '/Parent': 'Parent', '/T': 'Field Name', '/TU': 'Alternate Field Name', '/TM': 'Mapping Name', '/Ff': 'Field Flags', '/V': 'Value', '/DV': 'Default Value' }
+    fieldAttributes = {
+        '/FT': 'Field Type', 
+        '/Parent': 'Parent', 
+        '/T': 'Field Name', 
+        '/TU': 'Alternate Field Name',
+        '/TM': 'Mapping Name', 
+        '/Ff': 'Field Flags', 
+        '/V': 'Value', 
+        '/DV': 'Default Value'
+    }
     
     if retval is None:
         retval = OrderedDict()
         catalog = obj.trailer["/Root"]
-        # get the AcroForm tree
         if "/AcroForm" in catalog:
             tree = catalog["/AcroForm"]
         else:
@@ -47,7 +44,6 @@ def getformfields(obj, tree=None, retval=None, fileobj=None):
     obj._checkKids(tree, retval, fileobj)
     for attr in fieldAttributes:
         if attr in tree:
-            # Tree is a field
             obj._buildField(tree, retval, fileobj, fieldAttributes)
             break
 
@@ -60,34 +56,138 @@ def getformfields(obj, tree=None, retval=None, fileobj=None):
     return retval
 
 def getfields(fn):
-    infile = PdfFileReader(open(fn, 'rb'))
-    fields = getformfields(infile)
+    pdf = None
+    f = open(fn, 'rb')
+    pdf = PdfFileReader(f)
+    fields = getformfields(pdf)
+    f.close()
     return OrderedDict((k, v.get('/V', '')) for k, v in fields.items())
-    
 
-def gettextfields(i1, i2, fn):
-    # Instruction(s) to get the text fields goes here...
-    return
+def removeff(lst):
+    r = []
+    for l in lst:
+        if not '|' in l:
+            r.append(l)
+    return r
+
+def findtextfields(flds, l, nl):
+    res = []
+    for fld in flds:
+        fieldn = fld.split('=')[0]
+        fieldkw = fld.split('=')[1]
+        words = l.split(' ')
+        nwords = nl.split(' ')
+        for i, w in enumerate(words):
+            if fieldkw.lower() in w.lower():
+                res.append(fieldn + '|' + nwords[i])
+                break
+    return res
+
+def flatlst(lst):
+    return [item for sl in lst for item in sl]
+
+def dict(lst):
+    dic = OrderedDict()
+    items = flatlst(lst)
+    for itm in items:
+        k = itm.split('|')[0]
+        v = itm.split('|')[1]
+        dic[k] = v
+    return dic
+
+def gettextfields(kw, i1, i2, fn):
+    res = []
+    pdf = pdfplumber.open(fn)
+    page = pdf.pages[0]
+    pgtxt = page.extract_text()
+    usei1 = False
+
+    if kw.lower() in pgtxt.lower():
+        usei1 = True
+        txt = pgtxt.split('\n')
+        flds = removeff(list(set(i1 + i2)))
+        for i, l in enumerate(txt):
+            if len(l) > 100:
+                continue
+            r = findtextfields(flds, l, txt[i+1])
+            if len(r) > 0:
+                res.append(r)
+            if i == len(txt) - 2:
+                break
+            flt = flatlst(res)
+            if len(flt) == len(flds):
+                break
+        pdf.close()
+
+    return dict(res), usei1
+
+def selectlist():
+    return '''
+    function selectedIdx(s, v) {
+        for (var i = 0; i < s.options.length; i++) {
+            if (s.options[i].text == v) {
+            s.options[i].selected = true;
+            return; 
+            }
+        }
+    }
+    '''
+
+def createscript(usei1, i1, i2, fields, txtfields, fn):
+    # todo...
+    # the function selectlist must be called from here...
+    jsScript = ""
+
+    # add the function to handle working with select elements
+    jsScript += selectlist()
+    
+    if usei1:
+        for line in i1:
+            jsScript += getFieldJavaScript(line, fields, txtfields)
+    else:
+        for line in i2:
+            jsScript += getFieldJavaScript(line, fields, txtfields)
+
+    return jsScript 
+
+def getFieldJavaScript(line, fields, txtfields):
+    if line.__contains__("="):
+        lineParts = line.split("=")
+        fieldName = lineParts[0]
+        value = txtfields[fieldName]
+
+        return f"document.getElementById('{fieldName}').value = '{value}';\r\n"
+    elif line.__contains__("|"):
+        lineParts = line.split("|")
+        fieldName = lineParts[0]
+        fieldType = lineParts[1]
+        value = fields[fieldName]
+
+        if fieldType == "e":
+            return f"document.getElementById('{fieldName}').value = '{value}';\r\n"
+        else:
+            return f"selectedIdx(document.getElementById('{fieldName}'), '{value}');\r\n"  
+
+    # If we got here, assume a bad line and just return an empty string
+    return ""                 
 
 def execute():
-
-    resultsDict = {}
-
     try: 
-        i1 = readini('l1.ini') # Read the 1st .ini file
-        i2 = readini('l2.ini') # Read the 2nd .ini file
-        # Read fields for each file (instructions are missing here)
-        # Both getfields and gettextfields should be invoked
-        pdfsToProces = getfiles("C:\\Projects\\python_rpa\\formats")
+        i1 = readini('i1.ini')
+        i2 = readini('i2.ini')
+        files = getfiles()
+        for fn in files:
+            fields = getfields(fn)            
+            txtfields, usei1 = gettextfields('ALBARAN Nº', i1, i2, fn)
 
-        for pdf in pdfsToProces:
-            resultsDict[pdf] = getfields(pdf)
-            
-        print(resultsDict)
+            fileNameWithExtension = os.path.basename(fn)
+            fileName = fileNameWithExtension.split(".")[0]
+            script_file = open(f"{fileName}.txt", "w")
+            script_file.write(createscript(usei1, i1, i2, fields, txtfields, fn))
+            script_file.close()
 
     except BaseException as msg:
         print('Error occured: ' + str(msg))
 
-    
 if __name__ == '__main__':
     execute()
